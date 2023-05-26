0d9c4398a746d262e4d2b93e965286d40e6f5816
irfnrdh <kang.fand@gmail.com>
2023-05-26 09:38:51 +0700

    Update Setup

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
 from setuptools import setup, find_packages
 
+# Read the contents of README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='ssind',
-    version='1.0.0',
+    version='0.1.1',
+    description='Take Unlimited ScreenShot Automation and make a report',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/irfnrdh/ssind/issues',
+    author='Irfannur Diah',
+    author_email='irfnrdh@gmail.com',
+    license='AGPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='sample setuptools development',
+    project_urls={
+        'Documentation': 'https://github.com/irfnrdh/ssind/wiki',
+        'Source Code': 'https://github.com/irfnrdh/ssind',
+        'Issue Tracker': 'https://github.com/irfnrdh/ssind/issues',
+    },
     packages=find_packages(),
     install_requires=[
         'Flask',
         'pdfkit',
         'tqdm',
-        # Add any other dependencies required by your application
+        'click',
+        'selenium',
+        'requests'
     ],
     entry_points={
         'console_scripts': [
@@ -17,3 +43,4 @@ setup(
     },
     include_package_data=True,
 )
+
